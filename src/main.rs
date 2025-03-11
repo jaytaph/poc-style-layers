239891c75d78478ab6d5eadc5e8aba30302f4b53
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-03-11 15:21:08 +0100

    Without layers

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
-use std::sync::{Arc, Mutex};
 use crate::stylelist::CssStyleList;
 
 mod properties;
-mod arena;
 mod values;
 mod stylelist;
-mod layer;
 
 fn main() {
-    let arena = Arc::new(Mutex::new(arena::LayerArena::new()));
-
-    let root_sheet = CssStyleList::root(arena.clone());
+    let root_sheet = CssStyleList::root();
     println!("Root layer");
     println!("Root : {:?}", root_sheet.get_property(properties::BACKGROUND));
     println!("Root : {:?}", root_sheet.get_property(properties::WIDTH));
 
     println!("Next1 layer");
-    let mut next_sheet_1 = CssStyleList::new(&root_sheet);
+    let mut next_sheet_1 = root_sheet.create();
     next_sheet_1.set_property(properties::BACKGROUND, values::CssValue::Keyword("red".to_string()));
     println!("Next1: {:?}", next_sheet_1.get_property(properties::BACKGROUND));
     println!("Next1: {:?}", next_sheet_1.get_property(properties::WIDTH));
 
     println!("Next2 layer");
-    let mut next_sheet_2 = CssStyleList::new(&next_sheet_1);
+    let mut next_sheet_2 = next_sheet_1.create();
     next_sheet_2.set_property(properties::BACKGROUND, values::CssValue::Keyword("green".to_string()));
     println!("Next2: {:?}", next_sheet_2.get_property(properties::BACKGROUND));
     println!("Next2: {:?}", next_sheet_2.get_property(properties::WIDTH));
@@ -38,14 +33,8 @@ fn main() {
 
 
     println!("Next3 layer");
-    let mut next_sheet_3 = CssStyleList::new(&next_sheet_1);
+    let mut next_sheet_3 = next_sheet_1.create();
     next_sheet_3.set_property(properties::BACKGROUND, values::CssValue::Keyword("rebeccapurple".to_string()));
     println!("Next3: {:?}", next_sheet_3.get_property(properties::BACKGROUND));
     println!("Next3: {:?}", next_sheet_3.get_property(properties::WIDTH));
-
-    println!("Layers");
-    println!("{:?}", root_sheet.layers());
-    println!("{:?}", next_sheet_1.layers());
-    println!("{:?}", next_sheet_2.layers());
-    println!("{:?}", next_sheet_3.layers());
 }
--- src/stylelist.rs
+++ src/stylelist.rs
@@ -1,124 +1,85 @@
-use std::sync::{Arc, Mutex};
-use crate::arena::{LayerArena, LayerId, ROOT_LAYER_ID};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use crate::properties;
 use crate::values::CssValue;
 
-/// A stylelist is nothing more than the arena and a layer id. This layer id is where
-/// new properties gets added, while current properties are found in the parent layers
-/// of this layer.
-pub(crate) struct CssStyleList {
-    arena: Arc<Mutex<LayerArena>>,
-    layer_id: LayerId
+#[derive(Clone)]
+pub struct CssStyleList(Arc<RwLock<CssStyleListInner>>);
+
+struct CssStyleListInner {
+    props: HashMap<&'static str, CssValue>,
+    parent: Option<CssStyleList>,
 }
 
 impl CssStyleList {
-    /// Create a root layer
-    pub(crate) fn root(arena: Arc<Mutex<LayerArena>>) -> Self {
-        Self {
-            arena,
-            layer_id: ROOT_LAYER_ID
-        }
+    pub(crate) fn root() -> CssStyleList {
+        CssStyleList(Arc::new(RwLock::new(CssStyleListInner {
+            props: generate_default_style(),
+            parent: None,
+        })))
     }
 
-    /// Create a new stylelist based on the parent stylelist. It will contain a new layer with no
-    /// properties.
-    pub(crate) fn new(parent: &CssStyleList) -> Self {
-        let mut binding = parent.arena.lock().unwrap();
-        let layer_id = binding.new_layer(parent.layer_id);
-        let parent_layer = binding.get_layer_mut(layer_id).unwrap();
-
-        Self {
-            arena: parent.arena.clone(),
-            layer_id: parent_layer.layer_id
-        }
+    pub(crate) fn create(&self) -> Self {
+        CssStyleList(Arc::new(RwLock::new(CssStyleListInner {
+            props: HashMap::new(),
+            parent: Some(self.clone()),
+        })))
     }
 
-    /// Sets the given property onto the current stylelists layer
     pub(crate) fn set_property(&mut self, property_name: &'static str, value: CssValue) {
-        let mut arena = self.arena.lock().unwrap();
-        let layer = arena.get_layer_mut(self.layer_id).unwrap();
-
-        layer.set_property(property_name, value);
+        self.0.write().unwrap().props.insert(property_name, value);
     }
 
-    /// Retrieve a property. If not found in the current layer, it will recursively search the
-    /// parent layers until it finds the property or reaches the root layer. When not found, it will
-    /// return None.
     pub(crate) fn get_property(&self, property_name: &'static str) -> Option<CssValue> {
-        let arena = self.arena.lock().unwrap();
-
-        let mut cur_layer_id = self.layer_id;
-
-        loop {
-            let layer = arena.get_layer(cur_layer_id).unwrap();
-
-            match layer.get_property(property_name) {
-                Some(value) => return Some(value.clone()),
-                None => {}
+        let binding = self.0.read().unwrap();
+        match binding.props.get(property_name) {
+            Some(value) => Some(value.clone()),
+            None => {
+                match &binding.parent {
+                    Some(parent) => parent.get_property(property_name),
+                    None => None,
+                }
             }
-
-            // Break the loop when we reached the root layer
-            if layer.parent_layer_id == cur_layer_id {
-                break;
-            }
-
-            // Move to the parent layer
-            cur_layer_id = layer.parent_layer_id;
         }
-
-        None
-    }
-
-    pub(crate) fn layers(&self) -> Vec<LayerId> {
-        let arena = self.arena.lock().unwrap();
-        let mut cur_layer_id = self.layer_id;
-        let mut layers = vec![cur_layer_id];
-
-        loop {
-            let layer = arena.get_layer(cur_layer_id).unwrap();
-            if layer.parent_layer_id == cur_layer_id {
-                break;
-            }
-
-            cur_layer_id = layer.parent_layer_id;
-            layers.push(cur_layer_id);
-        }
-
-        layers
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::sync::{Arc, Mutex};
-    use crate::properties;
+fn generate_default_style() -> HashMap<&'static str, CssValue> {
+    let mut props = HashMap::new();
 
-    #[test]
-    fn test_new_stylelist() {
-        let arena = Arc::new(Mutex::new(LayerArena::new()));
+    props.insert(properties::BACKGROUND, CssValue::Keyword("white".to_string()));
+    props.insert(properties::BORDER, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_BOTTOM, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_LEFT, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_RIGHT, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_TOP, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_RADIUS, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::BORDER_COLOR, CssValue::Keyword("black".to_string()));
+    props.insert(properties::BORDER_STYLE, CssValue::Keyword("solid".to_string()));
+    props.insert(properties::BORDER_WIDTH, CssValue::Keyword("1px".to_string()));
+    props.insert(properties::COLOR, CssValue::Keyword("black".to_string()));
+    props.insert(properties::DISPLAY, CssValue::Keyword("block".to_string()));
+    props.insert(properties::FONT_FAMILY, CssValue::Keyword("Arial".to_string()));
+    props.insert(properties::FONT_SIZE, CssValue::Keyword("16px".to_string()));
+    props.insert(properties::FONT_STYLE, CssValue::Keyword("normal".to_string()));
+    props.insert(properties::FONT_WEIGHT, CssValue::Keyword("normal".to_string()));
+    props.insert(properties::HEIGHT, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::LINE_HEIGHT, CssValue::Keyword("1".to_string()));
+    props.insert(properties::MARGIN, CssValue::Keyword("0".to_string()));
+    props.insert(properties::MARGIN_BOTTOM, CssValue::Keyword("0".to_string()));
+    props.insert(properties::MARGIN_LEFT, CssValue::Keyword("0".to_string()));
+    props.insert(properties::MARGIN_RIGHT, CssValue::Keyword("0".to_string()));
+    props.insert(properties::MARGIN_TOP, CssValue::Keyword("0".to_string()));
+    props.insert(properties::PADDING, CssValue::Keyword("0".to_string()));
+    props.insert(properties::PADDING_BOTTOM, CssValue::Keyword("0".to_string()));
+    props.insert(properties::PADDING_LEFT, CssValue::Keyword("0".to_string()));
+    props.insert(properties::PADDING_RIGHT, CssValue::Keyword("0".to_string()));
+    props.insert(properties::PADDING_TOP, CssValue::Keyword("0".to_string()));
+    props.insert(properties::TEXT_ALIGN, CssValue::Keyword("left".to_string()));
+    props.insert(properties::TEXT_DECORATION, CssValue::Keyword("none".to_string()));
+    props.insert(properties::TEXT_TRANSFORM, CssValue::Keyword("none".to_string()));
+    props.insert(properties::WIDTH, CssValue::Keyword("100%".to_string()));
+    props.insert(properties::Z_INDEX, CssValue::Keyword("0".to_string()));
 
-        let stylelist = CssStyleList::root(arena.clone());
-        assert_eq!(stylelist.layer_id, ROOT_LAYER_ID);
-    }
-
-    #[test]
-    fn test_set_property() {
-        let arena = Arc::new(Mutex::new(LayerArena::new()));
-        let mut stylelist = CssStyleList::root(arena.clone());
-        stylelist.set_property(properties::BACKGROUND, CssValue::Keyword("red".to_string()));
-
-        let arena = arena.lock().unwrap();
-        let layer = arena.get_layer(stylelist.layer_id).unwrap();
-        assert_eq!(layer.get_property(properties::BACKGROUND), Some(&CssValue::Keyword("red".to_string())));
-    }
-
-    #[test]
-    fn test_get_property() {
-        let arena = Arc::new(Mutex::new(LayerArena::new()));
-        let mut stylelist = CssStyleList::root(arena.clone());
-        stylelist.set_property(properties::BACKGROUND, CssValue::Keyword("red".to_string()));
-
-        let value = stylelist.get_property(properties::BACKGROUND);
-        assert_eq!(value, Some(&CssValue::Keyword("red".to_string())));
-    }
+    props
 }
